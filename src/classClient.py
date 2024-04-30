84ea8657c245d506232783f976e7fb31342144d0
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-04-30 23:04:14 +0200

    created ClientClass and used it inside the client gui

---
+++ src/classClient.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+import threading
+import signal
+
+
+class Client:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, address, port):
+        try:
+            self.client.connect((address, port))
+            return True
+        except Exception as e:
+            print(f'Errore di connessione: {e}')
+            return False
+
+    def disconnect(self):
+        try:
+            self.client.close()
+        except Exception as e:
+            print(f'Errore di connessione: {e}')
+            sys.exit(0)
+
+    def printResponses(self):
+        try:
+            return self.client.recv(4096)
+        except Exception as e:
+            print(f'Errore di connessione: {e}')
+            sys.exit(0)
+          
+
+    def writeMessages(self, message):
+        try:
+            self.client.send(message.encode())
+        except Exception as e:
+            print(f'Errore di connessione: {e}')
+            sys.exit(0)
+
--- src/clientGui.py
+++ src/clientGui.py
@@ -1,40 +1,75 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from classClient import Client
+import threading
 
 class ChatClient:
-    def __init__(self, master):
+    def __init__(self, master, client):
         self.master = master
         self.master.title("Client")
+        self.client = client
         
         self.server_port = tk.Entry(master, width=40)
         self.server_port.grid(row=0, column=0, padx=10, pady=10)
         
-        self.connect_button = tk.Button(master, text="Connect", command=self.ciao)
+        self.connect_button = tk.Button(master, text="Connect", command=self.connect)
         self.connect_button.grid(row=0, column=1, padx=10, pady=10)
         
-        self.close_button = tk.Button(master, text="Close", command=self.ciao)
+        self.close_button = tk.Button(master, text="Disconnect", command=self.disconnect)
         self.close_button.grid(row=0, column=2, padx=10, pady=10)
+
+
+        self.status_text = tk.Label(master, text="Status:", bg="white", relief="sunken")
+        self.status_text.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
         
         self.chat_history = scrolledtext.ScrolledText(master, width=50, height=20)
-        self.chat_history.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+        self.chat_history.grid(row=2, column=0, padx=10, pady=10, columnspan=2)
         self.chat_history.config(state='disabled')
         
         self.message_entry = tk.Entry(master, width=40)
-        self.message_entry.grid(row=2, column=0, padx=10, pady=10)
         
-        self.send_button = tk.Button(master, text="Send", command=self.ciao)
-        self.send_button.grid(row=2, column=1, padx=10, pady=10)
-      
-      
-    def ciao(self):
-        print("ciao")
+        self.send_button = tk.Button(master, text="Send", command=self.write_message)
+       
+
+
+    def connect(self):
+        port = self.server_port.get()
+        if len(port) > 0:
+            connected = self.client.connect("localhost", int(port))
+            if connected:
+                self.status_text.config(text='connected')  # Abilita la modifica del testo
+                self.message_entry.grid(row=3, column=0, padx=10, pady=10)
+                self.send_button.grid(row=3, column=1, padx=10, pady=10)
+                thread = threading.Thread(target=self.print_response)
+                thread.daemon = True
+                thread.start()
+            else:
+                self.status_text.config(text='error with connection')  # Abilita la modifica del testo
+       
+
+    def disconnect(self):
+        self.client.disconnect()
+        self.status_text.config(text='disconnected')
+            
+    def write_message(self):
+        message=self.message_entry.get()
+        self.client.writeMessages(message)
+        self.chat_history.config(state='normal')  # Abilita la modifica del testo
+        self.chat_history.insert(tk.END, message+"\n")  # Aggiunge il messaggio alla fine della finestra di chat
+
+    def print_response(self):
+        while True:
+            response = self.client.printResponses()
+            self.chat_history.config(state='normal')
+            self.chat_history.insert(tk.END, response.decode() + "\n")
         
       
     
  
 def main():
     root = tk.Tk()
-    chat_client = ChatClient(root)
+    client = Client()
+    chat_client = ChatClient(root, client)
     root.mainloop()
 
 if __name__ == "__main__":
