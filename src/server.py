04d13763f7594a358deca479069ad6f1ab2701b1
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-05-01 14:05:49 +0200

    Added local buffers for client and now the server will read those buffers

--- src/server.py
+++ src/server.py
@@ -5,46 +5,55 @@ import signal
 import queue
 
 
+class Client:
+
+    def __init__(self, connectionSocket, addr):
+
+        self.connectionSocket = connectionSocket
+        self.addr = addr
+
+        self.local_messages = queue.Queue()
+        self.local_messages_lock = threading.Lock()
+
+        
+    def listenMessages(self):
+         while True:
+            response = self.connectionSocket.recv(4096)
+            with self.local_messages_lock:
+                self.local_messages.put(response)
+
+    def send(self, message):
+        self.connectionSocket.send(message)
+
+
 def addClients():
     while True:
         connectionSocket, addr = server.accept() 
+        client = Client(connectionSocket, addr)
         with clients_lock:
-            clients.append(connectionSocket)
-            threading.Thread(target=manageClient, args=(connectionSocket,)).start()
+            clients.append(client)
+            threading.Thread(target=client.listenMessages).start()
         print(f'Connected to {addr}')
 
-def manageClient(client):
-   addMessages(client)
 
-
-        
-       
-
-def addMessages(client):
-    local_queue = queue.Queue()
-    while True:
-        response = client.recv(4096)
-        print(response.decode())
-        with messages_condition:
-            messages.put((response, client))
-            messages_condition.notify()
             
-
-
 def sendMessages():
     while True:
-        with messages_condition:
-            while messages.empty():
-                messages_condition.wait()
-
-            if len(clients) > 0:
+        if messages.empty():
+            with clients_lock:
+                for client in clients:
+                    with client.local_messages_lock:
+                        while not client.local_messages.empty():
+                            response = client.local_messages.get()
+                            messages.put((response, client))
+        else:
                 with clients_lock:
                     while not messages.empty():
                         message, client_from_queue = messages.get()
                         for client in clients:
-                            if client == client_from_queue:
-                                continue
-                            client.send(message)
+                            if client != client_from_queue:
+                                client.send(message)
+                            
 
                 
                 
@@ -62,10 +71,7 @@ print("Server in ascolto su porta 8888...")
 # Lista per tenere traccia dei client connessi
 clients = []
 clients_lock = threading.Lock()
-
 messages = queue.Queue()
-messages_condition = threading.Condition()
-
 
 
 print ('Ready to serve...')
@@ -73,10 +79,12 @@ thread = threading.Thread(target=addClients)
 thread.daemon = True
 thread.start()
 
-
 thread2 = threading.Thread(target=sendMessages)
 thread2.daemon = True
 thread2.start()
+
+
+
 
 print('Waiting for clients...')
 #interrompe l’esecuzione se da tastiera arriva la sequenza (CTRL + C) 
